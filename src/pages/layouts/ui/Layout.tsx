39dc20401ee2dd734142bca2aace346c8b4a6151
reyy7707 <starshiiaman@gmail.com>
2024-08-12 00:28:08 +0600

    upload/navigate and verify state

--- src/pages/layouts/ui/Layout.tsx
+++ src/pages/layouts/ui/Layout.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Sidebar } from '../../../features/saidbar';
 import { Recommendations } from '../../recomendations';
 import { Playlists } from '../../playlist';
+import { useNavigate } from 'react-router-dom';
 import './Layout.css';
 
 const Layout: React.FC = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (localStorage.getItem('spotifyAccessToken')) {
+      navigate('/register')
+    } else {
+      console.log('verify');
+    }
+  }, [])
+
   return (
     <div className="layout">
       <Sidebar />
