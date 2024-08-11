db5346b10469b93065433f804004d724306c6b36
reyy7707 <starshiiaman@gmail.com>
2024-08-12 00:36:52 +0600

    update/user profile style

--- src/features/personal-area/ui/UserProfile.tsx
+++ src/features/personal-area/ui/UserProfile.tsx
@@ -33,38 +33,42 @@ const UserProfile: React.FC = () => {
   }, [accessToken]);
 
   return (
-    <div className="user-profile">
-      {userData ? (
-        <>
-          <div className="profile-header">
-            <img src={userData.images[0]?.url} alt="Profile" className="profile-picture" />
-            <h1 className="profile-name">{userData.display_name}</h1>
-            <p className="profile-email">{userData.email}</p>
-            <p className="profile-followers">
-              {userData.followers?.total} Followers
-            </p>
-            <p className="profile-product">
-              Product: {userData.product.charAt(0).toUpperCase() + userData.product.slice(1)}
-            </p>
-          </div>
-          <div className="profile-playlists1">
-            <h2>My Playlists</h2>
-            <ul className="playlist-list">
-              {playlists.map((playlist: any) => (
-                <li key={playlist.id} className="playlist-item1">
-                  <Link to={`/playlist/${playlist.id}`} className="playlist-link">
-                    <img src={playlist.images[0]?.url} alt={playlist.name} className="playlist-cover2" />
-                    <p className="playlist-name1">{playlist.name}</p>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
+    <>
+      <div className='user'>
+        <div className="user-profile">
+          {userData ? (
+            <>
+              <div className="profile-header">
+                <img src={userData.images[0]?.url} alt="Profile" className="profile-picture" />
+                <h1 className="profile-name">{userData.display_name}</h1>
+                <p className="profile-email">{userData.email}</p>
+                <p className="profile-followers">
+                  {userData.followers?.total} Followers
+                </p>
+                <p className="profile-product">
+                  Product: {userData.product.charAt(0).toUpperCase() + userData.product.slice(1)}
+                </p>
+              </div>
+              <div className="profile-playlists1">
+                <h2>My Playlists</h2>
+                <ul className="playlist-list">
+                  {playlists.map((playlist: any) => (
+                    <li key={playlist.id} className="playlist-item1">
+                      <Link to={`/playlist/${playlist.id}`} className="playlist-link">
+                        <img src={playlist.images[0]?.url} alt={playlist.name} className="playlist-cover2" />
+                        <p className="playlist-name1">{playlist.name}</p>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
